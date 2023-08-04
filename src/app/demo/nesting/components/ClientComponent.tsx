cab681811f07618e1014a9bcabdb2dba3c354770
Aurora Walberg <aurora.walberg@inmeta.no>
2023-08-04 15:36:30 +0200

    update

--- src/app/demo/nesting/components/ClientComponent.tsx
+++ src/app/demo/nesting/components/ClientComponent.tsx
@@ -5,8 +5,6 @@ import React, { useState } from 'react';
 export default function ClientComponent({ children }: { children: React.ReactNode }) {
   const [count, setCount] = useState(0);
 
-  console.log('Client rendering 2: this should only be printed on the client');
-
   return (
     <div className="m-5 bg-yellow-dark p-5">
       <div className="flex flex-row justify-between gap-2">
@@ -15,6 +13,7 @@ export default function ClientComponent({ children }: { children: React.ReactNod
           className="rounded border-2 border-purple px-2"
           onClick={() => {
             return setCount(count => {
+              console.log('Client rendering: this should only be printed on the client');
               return count + 1;
             });
           }}
--- src/app/demo/nesting/components/ServerComponent.tsx
+++ src/app/demo/nesting/components/ServerComponent.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function ServerComponent() {
-  const [count] = useState(0);
+  console.log('CHILD Server rendering: this should only be printed on the server');
 
-  console.log('CHILD Server rendering 3: this should only be printed on the server');
-
-  return <div className="m-5 bg-yellow p-5 text-purple">Server component {count}</div>;
+  return <div className="m-5 bg-yellow p-5 text-purple">Server component</div>;
 }
