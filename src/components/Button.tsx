df93232de61702f9b0fa321ce249d00f5b925b75
Aurora Walberg <aurora.walberg@inmeta.no>
2023-09-04 19:57:50 +0200

    update button coor classes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import { cn } from '../utils/style';
 
 type Props = {
   color?: string;
@@ -8,11 +9,13 @@ type Props = {
 
 export default function Button({ color, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement> & Props) {
   const backgroundColor = color || 'yellow';
+  const defaultClasses =
+    'inline-flex w-fit cursor-pointer items-center justify-center rounded px-5 py-3 font-display text-lg font-bold leading-none text-purple shadow-lg hover:transform hover:no-underline hover:enabled:-translate-y-px disabled:cursor-default disabled:opacity-80';
+  const colorClasses = `bg-${backgroundColor}`;
+  const className = cn(defaultClasses, colorClasses);
+
   return (
-    <button
-      className={`bg-${backgroundColor} inline-flex w-fit cursor-pointer items-center justify-center rounded px-5 py-3 font-display text-lg font-bold leading-none text-purple shadow-lg hover:transform hover:no-underline hover:enabled:-translate-y-px disabled:cursor-default disabled:opacity-80`}
-      {...props}
-    >
+    <button className={className} {...props}>
       {props.children}
     </button>
   );
