27f6b8e6745df0d41121688a5623ac5be657b015
Aurora Walberg <aurora.walberg@inmeta.no>
2023-09-23 16:29:38 +0200

    update deploy caching

--- src/app/demo/suspense/_actions/getJokes.ts
+++ src/app/demo/suspense/_actions/getJokes.ts
@@ -1,8 +1,10 @@
-'use server';
-
 import { prisma } from '@/db';
 
+export const revalidate = '0';
+
 export async function getJokes() {
+  'use server';
+
   await new Promise(resolve => {
     return setTimeout(resolve, 3000);
   });
