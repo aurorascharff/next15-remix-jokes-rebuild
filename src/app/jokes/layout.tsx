443a4b5c877547e21f0b3d83a93ea7720c774cad
Aurora Walberg <aurora.walberg@inmeta.no>
2023-10-04 22:48:24 +0200

    update

--- src/app/jokes/layout.tsx
+++ src/app/jokes/layout.tsx
@@ -1,8 +1,9 @@
 import Footer from '@/src/components/Footer';
 import Header from '@/src/components/Header';
+import Sidebar from '@/src/components/Sidebar';
 import type { Metadata } from 'next';
 
-// export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
   description: 'Jokes Rebuild',
@@ -13,7 +14,10 @@ export default function JokesLayout({ children }: { children: React.ReactNode })
   return (
     <main className="flex h-full flex-col gap-5 bg-purple">
       <Header />
-      <div className="mx-10 flex h-full flex-col gap-10 py-3 md:mx-40 md:flex-row">{children}</div>
+      <div className="mx-10 flex h-full flex-col gap-10 py-3 md:mx-40 md:flex-row">
+        <Sidebar />
+        <div className="flex-1">{children}</div>
+      </div>
       <Footer />
     </main>
   );
