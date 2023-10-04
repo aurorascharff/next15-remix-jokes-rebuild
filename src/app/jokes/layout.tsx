902dda883d5f42b0389e78ceff5da31c4a9632ad
Aurora Walberg <aurora.walberg@inmeta.no>
2023-10-04 22:43:50 +0200

    test something

--- src/app/jokes/layout.tsx
+++ src/app/jokes/layout.tsx
@@ -1,10 +1,9 @@
 import Footer from '@/src/components/Footer';
 import Header from '@/src/components/Header';
-import Sidebar from '@/src/components/Sidebar';
+
 import type { Metadata } from 'next';
 
-// export const dynamic = 'force-dynamic';
-export const revalidate = '0';
+export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
   description: 'Jokes Rebuild',
@@ -15,10 +14,7 @@ export default function JokesLayout({ children }: { children: React.ReactNode })
   return (
     <main className="flex h-full flex-col gap-5 bg-purple">
       <Header />
-      <div className="mx-10 flex h-full flex-col gap-10 py-3 md:mx-40 md:flex-row">
-        <Sidebar />
-        <div className="flex-1">{children}</div>
-      </div>
+      <div className="mx-10 flex h-full flex-col gap-10 py-3 md:mx-40 md:flex-row">{children}</div>
       <Footer />
     </main>
   );
