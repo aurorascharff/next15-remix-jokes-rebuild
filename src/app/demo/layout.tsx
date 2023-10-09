64eb7897611b4b306b95903f79e0b37b7e52b3a5
Aurora Walberg <aurora.walberg@inmeta.no>
2023-10-09 21:02:30 +0200

    css things

--- src/app/demo/layout.tsx
+++ src/app/demo/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function DemoLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-[100svh] flex-col gap-5 bg-purple py-4">
+    <div className="flex min-h-[100svh] w-full flex-col gap-5 bg-purple py-4">
       <Toaster position="top-right" />
       <NavBar
         routes={[
--- src/app/jokes/layout.tsx
+++ src/app/jokes/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function JokesLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex min-h-[100svh] flex-col gap-5 bg-purple">
+    <main className="flex min-h-[100svh] w-full flex-col gap-5 bg-purple">
       <Header />
       <div className="mx-10 flex flex-grow flex-col gap-10 py-3 md:mx-40 md:flex-row">
         <Sidebar />
