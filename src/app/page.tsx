c51f1cae7b3cbf795ee2af6a3191f3bb190c3a8d
Aurora Walberg <aurora.walberg@inmeta.no>
2023-10-19 10:28:53 +0200

    update a11y and og image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
         <h1 className="text-center font-display text-6xl text-shadow">
           Next <span className="block font-display text-6xl uppercase leading-none md:text-9xl">Jokes!</span>
         </h1>
-        <h3 className="pb-2 text-center font-display text-shadow">Next 13 rebuild of Remix Jokes</h3>
+        <h2 className="h3 pb-2 text-center font-display text-shadow">Next 13 rebuild of Remix Jokes</h2>
         <nav>
           <ul className="flex list-none gap-4 p-0 font-display text-lg leading-none">
             <li>
