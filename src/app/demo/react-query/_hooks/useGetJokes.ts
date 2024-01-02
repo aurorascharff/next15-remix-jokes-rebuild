f3ac95d63419887e746e940006c669bda45dc5cf
Aurora Walberg <aurora.walberg@inmeta.no>
2024-01-02 16:24:05 +0100

    Refactor useGetJokes to use arrow function in queryFn

--- src/app/demo/react-query/_hooks/useGetJokes.ts
+++ src/app/demo/react-query/_hooks/useGetJokes.ts
@@ -2,5 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { getJokes } from '@/src/actions/getJokes';
 
 export default function useGetJokes() {
-  return useQuery({ queryFn: getJokes, queryKey: ['jokes'] });
+  return useQuery({
+    queryFn: () => {
+      return getJokes();
+    },
+    queryKey: ['jokes'],
+  });
 }
