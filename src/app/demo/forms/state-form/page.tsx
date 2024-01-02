33e80d5e815f26430c9e69b4d7938f93de118462
Aurora Walberg <aurora.walberg@inmeta.no>
2024-01-02 21:33:37 +0100

    Update description for useFormState form

--- src/app/demo/forms/state-form/page.tsx
+++ src/app/demo/forms/state-form/page.tsx
@@ -6,7 +6,7 @@ export default async function FormStatePage() {
   return (
     <div className="flex flex-col gap-5 xl:w-1/3">
       <h4>FormState form</h4>
-      The useFormState hook can be used to manage form state without a library.
+      The useFormState hook can be used to manage form state and provide progressive enhancement.
       <StateForm />
       <JokesList />
     </div>
