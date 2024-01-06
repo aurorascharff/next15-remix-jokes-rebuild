839da7d61cc0e4f8ce9161eae37f2f541f6e644d
Aurora Walberg <aurora.walberg@inmeta.no>
2024-01-06 20:40:13 +0100

    Reset StateForm on success

--- src/app/demo/forms/_components/StateForm.tsx
+++ src/app/demo/forms/_components/StateForm.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useFormState } from 'react-dom';
 import toast from 'react-hot-toast';
 import SubmitButton from '@/src/components/SubmitButton';
 import { createJokeStateForm } from '../_actions/createJokeStateForm';
 
 export default function StateForm() {
+  const formRef = useRef<HTMLFormElement | null>(null);
+
   const [state, formAction] = useFormState(createJokeStateForm, {
     error: undefined,
     success: false,
@@ -14,12 +16,15 @@ export default function StateForm() {
 
   useEffect(() => {
     if (state?.success) {
+      if (formRef.current) {
+        formRef.current.reset();
+      }
       toast.success('Joke added!');
     }
   }, [state.success]);
 
   return (
-    <form action={formAction}>
+    <form action={formAction} ref={formRef}>
       <label>
         Name:
         <input name="name" type="text" />
--- src/app/demo/forms/page.tsx
+++ src/app/demo/forms/page.tsx
@@ -12,9 +12,9 @@ export default async function FormsPage() {
       <h4>Forms</h4>
       <NavButton href="/demo/forms/server">Server validation</NavButton>
       <NavButton href="/demo/forms/client">Client validation</NavButton>
-      <NavButton href="/demo/forms/state-form">FormState form</NavButton>
       <NavButton href="/demo/forms/optimistic">Optimistic update</NavButton>
       <NavButton href="/demo/forms/react-hook">React Hook Form</NavButton>
+      <NavButton href="/demo/forms/state-form">FormState form</NavButton>
     </div>
   );
 }
