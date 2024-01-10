d21d5a103b22b5f0d1aac0dbb4f1cd4b453a5912
Aurora Walberg <aurora.walberg@inmeta.no>
2024-01-10 08:04:04 +0100

    Delete files to be coded in demo

--- src/app/jokes/new/page.tsx
+++ src/app/jokes/new/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import SubmitButton from '@/src/components/SubmitButton';
-import { createJoke } from '@/src/lib/actions/createJoke';
+
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -8,22 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function NewJokePage() {
-  return (
-    <div className="flex flex-col gap-5">
-      <p>Add your own hilarious joke</p>
-      <form action={createJoke}>
-        <label>
-          Name:
-          <input name="name" type="text" required minLength={2} />
-        </label>
-        <label>
-          Content:
-          <textarea name="content" required minLength={5} />
-        </label>
-        <div className="flex justify-end">
-          <SubmitButton />
-        </div>
-      </form>
-    </div>
-  );
+  return <div className="flex flex-col gap-5">New joke page</div>;
 }
