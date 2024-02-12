43e5e0e04001983b2f3cdae62297bc8ad09ba752
Aurora Walberg <aurora.walberg@inmeta.no>
2024-02-12 12:17:54 +0100

    Add back generateMetadata

--- app/jokes/[jokeid]/page.tsx
+++ app/jokes/[jokeid]/page.tsx
@@ -2,10 +2,22 @@ import Link from 'next/link';
 import React from 'react';
 import DeleteJokeButton from '@/components/DeleteJokeButton';
 import { getJoke } from '@/lib/services/getJoke';
+import { Metadata } from 'next';
 
 type PageProps = {
   params: { jokeid: string };
 };
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const joke = await getJoke(params.jokeid);
+
+  return joke
+    ? {
+        description: `Enjoy the "${joke.name}" joke and much more`,
+        title: `"${joke.name}" joke`,
+      }
+    : { description: 'No joke found', title: 'No joke' };
+}
 
 export default async function JokePage({ params }: PageProps) {
   const joke = await getJoke(params.jokeid);
