af07fc2abaa067e303b4982958ab4b8485ff1202
Aurora Walberg <aurora.walberg@inmeta.no>
2024-02-21 18:06:21 +0100

    Simplify form by using the forms onBlur and onChange

--- app/demo/actions-transitions/_components/UpdateJokeForm.tsx
+++ app/demo/actions-transitions/_components/UpdateJokeForm.tsx
@@ -17,8 +17,8 @@ export default function UpdateJokeForm({ joke }: Props) {
     name: joke.name,
   });
 
-  const onBlur = (value: string, field: keyof JokeSchemaType) => {
-    if (joke[field] === value || value === '') {
+  const onBlur = (event: React.FocusEvent<HTMLFormElement, Element>) => {
+    if (joke[event.target.name as keyof JokeSchemaType] === event.target.value || event.target.value === '') {
       return;
     }
     startTransition(() => {
@@ -26,42 +26,21 @@ export default function UpdateJokeForm({ joke }: Props) {
     });
   };
 
-  const onChange = (value: string, field: keyof JokeSchemaType) => {
-    setActiveJoke({ ...activeJoke, [field]: value });
+  const onChange = (event: React.FocusEvent<HTMLFormElement, Element>) => {
+    setActiveJoke({ ...activeJoke, [event.target.name]: event.target.value });
   };
 
   return (
     <>
       <div className="flex flex-col gap-2">
-        <form autoComplete="off">
+        <form onBlur={onBlur} onChange={onChange} autoComplete="off">
           <label>
             Name
-            <input
-              disabled={isPending}
-              onBlur={e => {
-                return onBlur(e.target.value, 'name');
-              }}
-              onChange={e => {
-                return onChange(e.target.value, 'name');
-              }}
-              value={activeJoke.name}
-              name="name"
-              type="text"
-            />
+            <input disabled={isPending} value={activeJoke.name} name="name" type="text" />
           </label>
           <label>
             Content:
-            <textarea
-              disabled={isPending}
-              onChange={e => {
-                return onChange(e.target.value, 'content');
-              }}
-              onBlur={e => {
-                return onBlur(e.target.value, 'content');
-              }}
-              value={activeJoke.content}
-              name="content"
-            />
+            <textarea disabled={isPending} value={activeJoke.content} name="content" />
           </label>
         </form>
         <span> {isPending ? 'Saving joke...' : ''}</span>
