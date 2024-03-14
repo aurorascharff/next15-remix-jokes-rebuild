dc9701368f4a12b3bf121c4277077512e0637a6e
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-03-14 21:31:51 +0100

    Don't use hidden input, just bind

--- components/Favorite.tsx
+++ components/Favorite.tsx
@@ -5,29 +5,23 @@ import { favoriteJoke } from '@/lib/actions/favoriteJoke';
 import type { Joke } from '@prisma/client';
 
 export default function Favorite({ joke }: { joke: Joke }) {
-  const favoriteJokeById = favoriteJoke.bind(null, joke.id);
-  const favorite = joke.favorite;
-  const [optimisticFavorite, addOptimisticFavorite] = useOptimistic(favorite);
+  const favoriteJokeById = favoriteJoke.bind(null, joke.id, joke.favorite);
   const [, startTransition] = useTransition();
+  const [optimisticFavorite, setOptimisticFavorite] = useOptimistic(joke.favorite);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     startTransition(async () => {
-      addOptimisticFavorite(!favorite);
-      await favoriteJokeById(new FormData(event.currentTarget));
+      setOptimisticFavorite(!optimisticFavorite);
+      await favoriteJokeById();
     });
   };
 
   return (
-    <form className="w-fit" action={favoriteJokeById} onSubmit={onSubmit}>
-      <button
-        type="submit"
-        className="w-fit text-yellow-400"
-        aria-label={optimisticFavorite ? 'Remove from favorites' : 'Add to favorites'}
-      >
+    <form action={favoriteJokeById} onSubmit={onSubmit} className="w-fit">
+      <button type="submit" className="w-fit text-yellow-400">
         {optimisticFavorite ? '★' : '☆'}
       </button>
-      <input type="hidden" name="favorite" value={favorite ? 'false' : 'true'} />
     </form>
   );
 }
