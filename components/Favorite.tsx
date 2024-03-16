eebff389ba21331da5dc0c9dbf299c57d13d4668
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-03-16 18:18:17 +0100

    update favorite starter

--- components/Favorite.tsx
+++ components/Favorite.tsx
@@ -5,7 +5,7 @@ import { favoriteJoke } from '@/lib/actions/favoriteJoke';
 import type { Joke } from '@prisma/client';
 
 export default function Favorite({ joke }: { joke: Joke }) {
-  const favoriteJokeById = favoriteJoke.bind(null, joke.id);
+  const favoriteJokeById = favoriteJoke.bind(null, joke.id, joke.favorite);
 
   return (
     <form action={favoriteJokeById} className="w-fit">
--- lib/actions/favoriteJoke.ts
+++ lib/actions/favoriteJoke.ts
@@ -3,9 +3,9 @@
 import { revalidatePath } from 'next/cache';
 import { prisma } from '@/db';
 
-export async function favoriteJoke(jokeId: string) {
+export async function favoriteJoke(jokeId: string, isFavorite: boolean) {
   await prisma.joke.update({
-    data: { favorite: true },
+    data: { favorite: !isFavorite },
     where: { id: jokeId },
   });
   revalidatePath('/jokes');
