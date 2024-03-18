3bc875af93f0712c6333845c1b76c3cd7b2d5c26
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-03-18 20:39:46 +0100

    Step 4

--- components/JokeForm.tsx
+++ components/JokeForm.tsx
@@ -1,18 +1,28 @@
 'use client';
 
 import React from 'react';
+import { useFormState } from 'react-dom';
 import { createJoke } from '@/lib/actions/createJoke';
+import type { JokeSchemaErrorType } from '@/validations/jokeSchema';
 import AddButton from './AddButton';
 
 export default function JokeForm() {
+  const [state, formAction] = useFormState(createJoke, {
+    errors: {} as JokeSchemaErrorType,
+    message: '',
+    success: false,
+  });
+
   return (
-    <form action={createJoke}>
+    <form action={formAction}>
       <label>
         Name: <input type="text" name="name" />
+        <span className="text-red">{state.errors?.fieldErrors?.name}</span>
       </label>
       <label>
         Content:
         <textarea name="content" />
+        <span className="text-red">{state.errors?.fieldErrors?.content}</span>
       </label>
       <AddButton />
     </form>
--- lib/actions/createJoke.ts
+++ lib/actions/createJoke.ts
@@ -3,23 +3,42 @@
 import { revalidatePath } from 'next/cache';
 import { prisma } from '@/db';
 import { slow } from '@/utils/slow';
+import type { JokeSchemaErrorType } from '@/validations/jokeSchema';
 import { jokeSchema } from '@/validations/jokeSchema';
 
-export async function createJoke(formData: FormData) {
-  await slow();
+type State = {
+  message?: string;
+  success: boolean;
+  errors?: JokeSchemaErrorType;
+};
 
+export async function createJoke(_prevState: State, formData: FormData) {
+  await slow();
   const result = jokeSchema.safeParse({
-    content: formData.get('content'),
-    name: formData.get('name'),
+    content: formData.get('content') as string,
+    name: formData.get('name') as string,
   });
 
   if (!result.success) {
-    console.log('VALIDATION ERROR');
-    return;
+    return {
+      errors: result.error.formErrors,
+      message: 'VALIDATION ERROR',
+      success: false,
+    };
   }
 
-  await prisma.joke.create({
-    data: result.data,
-  });
+  try {
+    await prisma.joke.create({
+      data: result.data,
+    });
+  } catch (error) {
+    return {
+      message: 'DATABASE ERROR',
+      success: false,
+    };
+  }
   revalidatePath('/jokes');
+  return {
+    success: true,
+  };
 }
