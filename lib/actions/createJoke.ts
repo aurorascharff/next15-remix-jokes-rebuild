bfb44f93f9125cca58509ab818d28e57e5528cf3
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-03-18 20:37:47 +0100

    Step 3

--- lib/actions/createJoke.ts
+++ lib/actions/createJoke.ts
@@ -2,13 +2,24 @@
 
 import { revalidatePath } from 'next/cache';
 import { prisma } from '@/db';
+import { slow } from '@/utils/slow';
+import { jokeSchema } from '@/validations/jokeSchema';
 
 export async function createJoke(formData: FormData) {
+  await slow();
+
+  const result = jokeSchema.safeParse({
+    content: formData.get('content'),
+    name: formData.get('name'),
+  });
+
+  if (!result.success) {
+    console.log('VALIDATION ERROR');
+    return;
+  }
+
   await prisma.joke.create({
-    data: {
-      content: formData.get('content') as string,
-      name: formData.get('name') as string,
-    },
+    data: result.data,
   });
   revalidatePath('/jokes');
 }
