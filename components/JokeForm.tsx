4baa1c7b642cc2fe930a0b9de0c63b6b7b681ec5
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-03-18 20:40:13 +0100

    Step 5

--- components/JokeForm.tsx
+++ components/JokeForm.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useFormState } from 'react-dom';
+import toast from 'react-hot-toast';
 import { createJoke } from '@/lib/actions/createJoke';
 import type { JokeSchemaErrorType } from '@/validations/jokeSchema';
 import AddButton from './AddButton';
@@ -12,9 +13,19 @@ export default function JokeForm() {
     message: '',
     success: false,
   });
+  const ref = React.useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    if (state.success) {
+      ref.current?.reset();
+      toast.success('Joke added');
+    } else if (state.message === 'Database error') {
+      toast.error('Failed to create joke...');
+    }
+  }, [state.message, state.success]);
 
   return (
-    <form action={formAction}>
+    <form ref={ref} action={formAction}>
       <label>
         Name: <input type="text" name="name" />
         <span className="text-red">{state.errors?.fieldErrors?.name}</span>
@@ -25,6 +36,7 @@ export default function JokeForm() {
         <span className="text-red">{state.errors?.fieldErrors?.content}</span>
       </label>
       <AddButton />
+      <noscript>{state.message === 'Database error' && <p className="text-red">Failed to create joke...</p>}</noscript>
     </form>
   );
 }
