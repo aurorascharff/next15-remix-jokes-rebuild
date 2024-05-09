e1cd2565e41132bbfb8a091e29f3f8d465fbb8e5
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-05-09 15:42:02 +0200

    Remove useless use()

--- app/demo/suspense/_components/JokesHeader.tsx
+++ app/demo/suspense/_components/JokesHeader.tsx
@@ -1,10 +1,8 @@
-import React, { use } from 'react';
+import React from 'react';
 
-export default function JokesHeader() {
-  use(
-    new Promise(resolve => {
-      return setTimeout(resolve, 2000);
-    }),
-  );
+export default async function JokesHeader() {
+  await new Promise(resolve => {
+    return setTimeout(resolve, 2000);
+  });
   return <h5>Jokes</h5>;
 }
