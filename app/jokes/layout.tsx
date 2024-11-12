2fb8f3afdec600e34a041dcce50a2cff486a392b
Aurora Scharff <aurora.sofie@gmail.com>
2024-11-12 12:50:22 +0100

    Move header and footer outside main

--- app/jokes/layout.tsx
+++ app/jokes/layout.tsx
@@ -7,13 +7,13 @@ export const dynamic = 'force-dynamic';
 
 export default function JokesLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex min-h-[100svh] w-full flex-col gap-5 bg-purple">
+    <div className="flex min-h-[100svh] w-full flex-col gap-5 bg-purple">
       <Header />
-      <div className="mx-10 flex grow flex-col gap-10 py-3 md:flex-row lg:mx-40">
+      <main className="mx-10 flex grow flex-col gap-10 py-3 md:flex-row lg:mx-40">
         <Sidebar />
         <div className="grow">{children}</div>
-      </div>
+      </main>
       <Footer />
-    </main>
+    </div>
   );
 }
